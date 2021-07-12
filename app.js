b59a0cd84c036b64dd14e4295a4430c82737a082
Farhan Hilmi <farhanhilmi077@gmail.com>
2021-07-12 23:54:10 +0700

    complete rest api module

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
 // const express = require('express');
 import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 // const feedRoutes = require('./routes/feed');
 import feedRoutes from './routes/feed.js';
+import authRoutes from './routes/auth.js';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, callback) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+};
 
 app.use(express.json());
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'),
+);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,5 +55,21 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8080);
+app.use((error, req, res, nest) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://farhanh:UH4udQV8YCO6vT06@cluster0.vstco.mongodb.net/messages?retryWrites=true&w=majority',
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,42 @@
 import express from 'express';
+import { body } from 'express-validator';
 
-import { getPosts, createPost } from '../controllers/feed.js';
+import isAuth from '../middleware/is-auth.js';
+
+import {
+  getPosts,
+  createPost,
+  getPost,
+  updatePost,
+  deletePost,
+} from '../controllers/feed.js';
 
 const router = express.Router();
 
-router.get('/posts', getPosts);
-router.post('/post', createPost);
+router.get('/posts', isAuth, getPosts);
+router.post(
+  '/post',
+  isAuth,
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  createPost,
+);
+
+router.get('/post/:postId', isAuth, getPost);
+
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  updatePost,
+);
+
+router.delete('/post/:postId', isAuth, deletePost);
 
 // module.exports = router;
 export default router;
